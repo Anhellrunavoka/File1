3532a2bd1be8e5bfe3f4aaee5e27d7c03cef99e2
Лина <@DESKTOP-DL5TO4I>
2024-06-19 17:30:50 +0300

    Добавьте файлы проекта.

---
+++ 4.2/4.2.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    cout << "Work with files\n";
+    //write information in file
+    /*ofstream file("text.txt");
+    if (!file) {
+        cout << "File not find\n";
+        return 1;
+    }
+    else {
+        cout << "File find\n";
+    }
+    cout << "Input information for write in file.For stop input *\n";
+    char txt[20];
+    int row = 1;
+    do {
+        cout << row << ")";
+        row++;
+        cin.getline(txt, 20);
+        file << txt << endl;
+    } while (*txt!='*');
+    */
+    //read file 
+    //ifstream file("text.txt");
+    ////if (!file) {
+    //if (file.is_open()) {
+    //    cout << "File not find\n";
+    //    return 1;
+    //}
+    //char txt[20];
+    //do {
+    //    file.getline(txt, 20);
+    //    cout << txt << endl;
+    //} while (file);
+    /*ofstream file("text.txt",ios::app);
+    if (!file) {
+        cout << "File not find\n";
+        return 1;
+    }
+    cout << "Input information for dowrite in file.For stop input *\n";
+    char txt[20];
+    int row = 1;
+    do {
+        cout << row << ")";
+        row++;
+        cin.getline(txt, 20);
+        file << txt << endl;
+    } while (*txt != '*');
+    cout << "Dowrite file complete\n";
+    file.close();*/
+    /*ifstream file("read.txt");
+    if (!file) {
+        cout << "File not find\n";
+        return 1;
+    }
+    ofstream write("write.txt");
+    if (!write) {
+        cout << "File not find\n";
+        return 1;
+    }
+    string word;
+    char chara[37];
+    do {
+        file.getline(chara,37);
+        for (char i : chara) {
+            if (i != ' ') {
+                word += i;
+            }
+            else if (i == ' ') {
+                if (word.length() >= 7) {
+                    write << word << " ";
+                }
+                word = "";
+            }
+        }
+    } while (file);
+    write.close();
+    file.close();
+    cout << "End";*/
+    ifstream file("text.txt");
+    if (!file) {
+        cout << "File not find\n";
+        return 1;
+    }
+    char txt[20];
+    string space = "";
+    while (file) {
+        file.getline(txt, 20);
+        //cout << txt << endl;
+        for (char i : txt) {
+            if (isdigit(i)) {
+                space += i;
+            }
+            if (i == ' ') {
+                cout << space << ' ';
+                space = "";
+            }
+        }
+    } 
+}
